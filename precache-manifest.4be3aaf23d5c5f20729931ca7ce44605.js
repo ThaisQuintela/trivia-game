941f4c4ba2d058620e8eb22b55e73044a6c1defe
Thais Quintela <thais.s.quintela@gmail.com>
2021-04-17 00:56:42 -0300

    Updates

---
+++ precache-manifest.4be3aaf23d5c5f20729931ca7ce44605.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ea4acbcbd1bb66f68f57a6136f0f12",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "46e3f9bfdcba36f84075",
+    "url": "/trivia-game/static/css/main.5747ac6e.chunk.css"
+  },
+  {
+    "revision": "f04354a5ede2d6a5a242",
+    "url": "/trivia-game/static/js/2.3c3d2ebc.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia-game/static/js/2.3c3d2ebc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46e3f9bfdcba36f84075",
+    "url": "/trivia-game/static/js/main.ce3af109.chunk.js"
+  },
+  {
+    "revision": "28c450d90070e1cbeaea",
+    "url": "/trivia-game/static/js/runtime-main.34810379.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-game/static/media/trivia.466d153e.png"
+  }
+]);
